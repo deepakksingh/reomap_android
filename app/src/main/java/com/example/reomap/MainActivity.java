6f18411e24969fbefa1aecc9c4f6a26fa65b02c4
Deepak Kumar Singh <deepak.singh@research.iiit.ac.in>
2020-03-11 16:39:56 +0530

    updated Initial Commit

---
+++ app/src/main/java/com/example/reomap/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.reomap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action",null)
+                        .show();
+            }
+        });
+    }
+}
